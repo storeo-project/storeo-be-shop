973309d8e230f9dc4701a7853b5a310683367a7a
minhdtb <minhdtb.83@gmail.com>
2022-02-23 14:10:33 +0700

    update coding

--- storeo-shop/src/main/resources/db/migration/V1_1_0__initial.sql
+++ storeo-shop/src/main/resources/db/migration/V1_1_0__initial.sql
@@ -1,11 +1,17 @@
 
-CREATE TABLE storeo_shop
+CREATE TABLE storeo_image
 (
     id          VARCHAR(255) NOT NULL,
     name        VARCHAR(255),
     description VARCHAR(255),
-    deleted     BOOLEAN,
+    type        INTEGER,
     created_at  TIMESTAMP WITHOUT TIME ZONE,
     updated_at  TIMESTAMP WITHOUT TIME ZONE,
-    CONSTRAINT pk_storeo_shop PRIMARY KEY (id)
+    CONSTRAINT pk_storeo_image PRIMARY KEY (id)
 );
+
+ALTER TABLE storeo_shop
+    ADD image_id VARCHAR(255);
+
+ALTER TABLE storeo_shop
+    ADD CONSTRAINT FK_STOREO_SHOP_ON_IMAGE FOREIGN KEY (image_id) REFERENCES storeo_image (id);
